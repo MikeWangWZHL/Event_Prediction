2999948cd9a80b110bafd4dbcc5ddf2b3652c74e
Mike Wang <wangz3@illinois.edu>
2020-07-19 11:47:34 -0500

    without need to use wec auth each time

--- Pydirve_try/README.md
+++ Pydirve_try/README.md
@@ -1,1 +1,2 @@
-add your "client_secrets.json" to the same folder as uploader.py
+1, follow the instructions in https://pythonhosted.org/PyDrive/quickstart.html to get the client_secrets.json file
+2, add your "client_secrets.json" to the same folder as uploader.py
--- Pydirve_try/uploader.py
+++ Pydirve_try/uploader.py
@@ -1,7 +1,21 @@
 from pydrive.auth import GoogleAuth
 
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
+
 
 from pydrive.drive import GoogleDrive
 
