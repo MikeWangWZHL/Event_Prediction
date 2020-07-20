d4bfea05e0a1a0428a1e3d76b1ab02e2b279fe96
Mike Wang <wangz3@illinois.edu>
2020-07-20 11:19:59 -0500

    add extract from directory function and create log file function

--- Pydirve_try/uploader.py
+++ Pydirve_try/uploader.py
@@ -1,4 +1,13 @@
 from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+from os import listdir
+from os.path import isfile, join, basename, split, abspath
+
+from pdf2image import convert_from_path, convert_from_bytes
+import pytesseract
+from PIL import Image
+import json
 
 gauth = GoogleAuth()
 
@@ -17,7 +26,6 @@ else:
 gauth.SaveCredentialsFile("mycreds.txt")
 
 
-from pydrive.drive import GoogleDrive
 
 # upload one file and get download link
 def uploadFileGetUrl(gauth, path, name):
@@ -33,9 +41,6 @@ def uploadFileGetUrl(gauth, path, name):
 
 
 # upload all files in a directory and get a list of download links:
-from os import listdir
-from os.path import isfile, join
-
 def uploadFiles(gauth, directory):
     # drive = GoogleDrive(gauth)
     files = [f for f in listdir(directory) if isfile(join(directory,f))]
@@ -45,9 +50,54 @@ def uploadFiles(gauth, directory):
     return urls # return a list of tuples [(filename, download link),]
 
 
+# extract text from one xxx.pdf and save it as xxx.txt at the same folder, return name of the text file
+def extractTextFromPdf(path):
+    img_list = convert_from_path(path)
+
+    whole_text = ""
+    print(f'start converting {path} ...')
+    count = 1
+    for img in img_list:
+        # img.show()
+        str_gen = pytesseract.image_to_string(img)
+        whole_text += str_gen
+        print("finish page", count)
+        count+=1
+    
+    filename = basename(path)[:-4] + '.txt'
+    print("write to",filename)
+
+    with open(filename,'w+') as f:
+        json.dump(whole_text,f)
+    
+    # return abspath(filename)
+    return filename
+
+def extractTextFromDirectory(directory):
+    files = [f for f in listdir(directory) if isfile(join(directory,f))]
+    file_list = []
+    for f in files:
+        file_list.append(extractTextFromPdf(join(directory,f)))
+    return file_list
+
+def createLog(urls,extracted_files):
+    with open('Log.txt','w+') as f:
+        pairs = {}
+        for i in range(len(urls)):
+            pairs[urls[i][0]] = (urls[i][1],extracted_files[i])
+        json.dump(pairs,f)
 # sample usage
-directory = "./pdf_test_data"
-print(uploadFiles(gauth,directory))
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+# directory = "./pdf_test_data"
+# print(uploadFiles(gauth,directory))
+
+# print(extractTextFromPdf('./pdf_test_data/multipage_small.pdf'))
+# print(extractTextFromDirectory('./pdf_test_data'))
+
+# urls = [(i,"hello") for i in range(5)]
+# extracted_text = extractTextFromDirectory('./pdf_test_data')
+# createLog(urls,extracted_text)
 
 
 
